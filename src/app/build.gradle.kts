d2ea54623458d0e80310577b4a85331e5918065a
Kolappan N <kolappannathan@users.noreply.github.com>
2026-07-23 01:52:12 +0530

    v2.0.0

--- src/app/build.gradle.kts
+++ src/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kolappan.aarathana"
         minSdk = 31
         targetSdk = 36
-        versionCode = 10202
-        versionName = "1.2.2"
+        versionCode = 20000
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
